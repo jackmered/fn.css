fd6508a6443f3eb27fbb315b2f9782b844059420
Jack Meredith <jackmered@gmail.com>
2020-01-29 22:05:44 +0000

    Added clean npm task and decreased new style components verbosity

--- README.md
+++ README.md
@@ -7,8 +7,10 @@
 - [ ] Decrease code verbosity
 - [x] Move from gulp to npm scripts
 - [ ] Implement lint in npm scripts
-- [ ] Implement clean dist in npm scripts
+- [x] Implement clean dist in npm scripts
 - [ ] Add styles for box shadow
+- [ ] Update styles for line height
+- [ ] Update styles for font sizing
 - [ ] Improve/finish docs
 - [ ] Include fn.min.css
 - [ ] Use autoprefixer with browserlist
--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,26 @@
 module.exports = {
   plugins: [
-    require("postcss-import"),
-    require("postcss-at-rules-variables"),
-    require("postcss-each"),
-    require("postcss-conditionals"),
-    require("postcss-css-variables")({ preserve: true }),
-    require("postcss-custom-media"),
-    require("postcss-nested"),
-    require("perfectionist")({
+    require('postcss-import'),
+    require('postcss-at-rules-variables'),
+    require('postcss-each'),
+    require('postcss-conditionals'),
+    require('postcss-css-variables')({ preserve: true }),
+    require('postcss-custom-media'),
+    require('postcss-nested'),
+    require('postcss-remove-media-query-ranges')({
+      min: 0,
+      removeMin: true,
+    }),
+    require('postcss-selector-replace')({
+      before: ['.xs-'],
+      after: ['.'],
+    }),
+    require('css-mqpacker'),
+    require('perfectionist')({
       cascade: false,
       indentSize: 2,
       trimLeadingZero: false,
-      maxSelectorLength: 1
-    })
-  ]
+      maxSelectorLength: 1,
+    }),
+  ],
 };
